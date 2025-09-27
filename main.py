15e12e7b7481ffaeeb4f4d0c20e7d501abbad629
Nipun Kakkar <mrnipunkakkar@gmail.com>
2025-09-27 16:28:45 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-
+'''
 STAGE_NAME="Data Transformation stage"
 
 try:
@@ -50,3 +50,4 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+'''
